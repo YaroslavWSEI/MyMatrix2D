a1477023c1facd513287120b1ff1361fd2f3613a
Yaroslav Furmanov <yaroslav.furmanov@microsoft.wsei.edu.pl>
2025-03-23 21:06:42 +0100

    Ukonczone testy + consoleapp

--- Matrix2dLib/Matrix2dConsoleApp/Program.cs
+++ Matrix2dLib/Matrix2dConsoleApp/Program.cs
@@ -1,3 +1,49 @@
 ﻿using Matrix2dLibr;
-var m = new Matrix2d();
-Console.WriteLine(m);
+// Tworzenie macierzy jednostkowej
+var identity = Matrix2d.Id;
+Console.WriteLine("Macierz jednostkowa:");
+Console.WriteLine(identity);
+// Tworzenie macierzy zerowej
+var zero = Matrix2d.Zero;
+Console.WriteLine("\nMacierz zerowa:");
+Console.WriteLine(zero);
+// Tworzenie macierzy z podanymi wartosciami
+var m1 = new Matrix2d(1, 2, 3, 4);
+var m2 = new Matrix2d(5, 6, 7, 8);
+Console.WriteLine("\nMacierz m1:");
+Console.WriteLine(m1);
+Console.WriteLine("\nMacierz m2:");
+Console.WriteLine(m2);
+// Dodawanie macierzy
+var sum = m1 + m2;
+Console.WriteLine("\nDodawanie m1 + m2:");
+Console.WriteLine(sum);
+// Odejmowanie macierzy
+var diff = m1 - m2;
+Console.WriteLine("\nOdejmowanie m1 - m2:");
+Console.WriteLine(diff);
+// Mnożenie macierzy
+var product = m1 * m2;
+Console.WriteLine("\nMnozenie m1 * m2:");
+Console.WriteLine(product);
+// Mnożenie macierzy przez skalar
+var scalarProduct = 2 * m1;
+Console.WriteLine("\nMnozenie m1 przez skalar 2:");
+Console.WriteLine(scalarProduct);
+// Transpozycja macierzy
+var transpose = Matrix2d.Transpose(m1);
+Console.WriteLine("\nTranspozycja m1:");
+Console.WriteLine(transpose);
+// Wyznacznik macierzy
+var determinant = m1.Determinant();
+Console.WriteLine("\nWyznacznik m1:");
+Console.WriteLine(determinant);
+// Parsowanie macierzy z ciągu znakow
+var parsedMatrix = Matrix2d.Parse("[[1, 2], [3, 4]]");
+Console.WriteLine("\nParsowanie macierzy z ciagu znakow:");
+Console.WriteLine(parsedMatrix);
+// Jawna konwersja macierzy na tablice
+var array = (int[,])m1;
+Console.WriteLine("\nJawna konwersja m1 na tablice:");
+Console.WriteLine($"[{array[0, 0]}, {array[0, 1]}]");
+Console.WriteLine($"[{array[1, 0]}, {array[1, 1]}]");
--- Matrix2dLib/Matrix2dTest/Test1.cs
+++ Matrix2dLib/Matrix2dTest/Test1.cs
@@ -22,7 +22,92 @@ namespace Matrix2dTest
             var zero = Matrix2d.Zero;
             Assert.AreEqual(new Matrix2d(0, 0, 0, 0), zero);
         }
+        [TestMethod]
+        // Sprawdzenie dodawania macierzy
+        public void TestDodawanie()
+        {
+            var m1 = new Matrix2d(1, 2, 3, 4);
+            var m2 = new Matrix2d(5, 6, 7, 8);
+            var result = m1 + m2;
+            Assert.AreEqual(new Matrix2d(6, 8, 10, 12), result);
+        }
 
-       
+        [TestMethod]
+        // Sprawdzenie odejmowania macierzy
+        public void TestOdejmowanie()
+        {
+            var m1 = new Matrix2d(5, 6, 7, 8);
+            var m2 = new Matrix2d(1, 2, 3, 4);
+            var result = m1 - m2;
+            Assert.AreEqual(new Matrix2d(4, 4, 4, 4), result);
+        }
+
+        [TestMethod]
+        // Sprawdzenie mnożenia macierzy
+        public void TestMnozenie()
+        {
+            var m1 = new Matrix2d(1, 2, 3, 4);
+            var m2 = new Matrix2d(2, 0, 1, 2);
+            var result = m1 * m2;
+            Assert.AreEqual(new Matrix2d(4, 4, 10, 8), result);
+        }
+
+        [TestMethod]
+        // Sprawdzenie mnożenia macierzy przez skalar
+        public void TestMnozeniePrzezSkalar()
+        {
+            var m = new Matrix2d(1, 2, 3, 4);
+            var result = 2 * m;
+            Assert.AreEqual(new Matrix2d(2, 4, 6, 8), result);
+        }
+
+        [TestMethod]
+        // Sprawdzenie transpozycji macierzy
+        public void TestTranspozycja()
+        {
+            var m = new Matrix2d(1, 2, 3, 4);
+            var result = Matrix2d.Transpose(m);
+            Assert.AreEqual(new Matrix2d(1, 3, 2, 4), result);
+        }
+
+        [TestMethod]
+        // Sprawdzenie wyznacznika macierzy
+        public void TestWyznacznik()
+        {
+            var m = new Matrix2d(1, 2, 3, 4);
+            var result = m.Determinant();
+            Assert.AreEqual(-2, result);
+        }
+
+        [TestMethod]
+        // Sprawdzenie parsowania macierzy z ciągu znaków
+        public void TestParsowanie()
+        {
+            var s = "[[1, 2], [3, 4]]";
+            var result = Matrix2d.Parse(s);
+            Assert.AreEqual(new Matrix2d(1, 2, 3, 4), result);
+        }
+
+        [TestMethod]
+        // Sprawdzenie niepoprawnego parsowania macierzy z ciągu znaków
+        [ExpectedException(typeof(FormatException))]
+        public void TestNiepoprawneParsowanie()
+        {
+            var s = "[[1, 2] [3, 4]]";
+            Matrix2d.Parse(s);
+        }
+
+        [TestMethod]
+        // Sprawdzenie jawnej konwersji macierzy na tablicę
+        public void TestJawnaKonwersja()
+        {
+            var m = new Matrix2d(1, 2, 3, 4);
+            var array = (int[,])m;
+            Assert.AreEqual(1, array[0, 0]);
+            Assert.AreEqual(2, array[0, 1]);
+            Assert.AreEqual(3, array[1, 0]);
+            Assert.AreEqual(4, array[1, 1]);
+        }
+
     }
 }
