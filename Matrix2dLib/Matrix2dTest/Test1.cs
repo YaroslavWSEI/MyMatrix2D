bf9369cbdaf141490d3c28204e8bd87b8c25970a
Yaroslav Furmanov <yaroslav.furmanov@microsoft.wsei.edu.pl>
2025-03-23 19:19:54 +0100

    Opracowanie testow

--- Matrix2dLib/Matrix2dTest/Test1.cs
+++ Matrix2dLib/Matrix2dTest/Test1.cs
@@ -1,11 +1,28 @@
-﻿namespace Matrix2dTest
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Matrix2dLibr;
+using System;
+
+namespace Matrix2dTest
 {
     [TestClass]
     public sealed class Test1
     {
         [TestMethod]
-        public void TestMethod1()
+        // Sprawdzenie macierzy jednostkowej
+        public void TestMacierzJednostkowa()
         {
+            var identity = Matrix2d.Id;
+            Assert.AreEqual(new Matrix2d(1, 0, 0, 1), identity);
         }
+
+        [TestMethod]
+        // Sprawdzenie zerowej macierzy
+        public void TestMacierzZerowa()
+        {
+            var zero = Matrix2d.Zero;
+            Assert.AreEqual(new Matrix2d(0, 0, 0, 0), zero);
+        }
+
+       
     }
 }
